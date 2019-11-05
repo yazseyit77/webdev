50a703eef7b682972090a83e087e6f32f2725308
yazseyit77 <yazgeldi.seyidov@gmail.com>
2019-11-05 14:52:27 -0800

    built index page and action

--- app/controllers/developers_controller.rb
+++ app/controllers/developers_controller.rb
@@ -1,3 +1,29 @@
 class DevelopersController < ApplicationController
-    
+    def index
+        @dev = Developers.all
+    end
+
+    def new
+
+    end
+
+    def create
+
+    end
+
+    def show
+
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :developers
+  resources :developers, only: [:index] 
   resources :clients
   resources :projects
 end
