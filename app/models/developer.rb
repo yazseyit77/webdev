387777e3d50940c6fded0adce1352e8f1c553914
yazseyit77 <yazgeldi.seyidov@gmail.com>
2019-11-06 14:33:46 -0800

    installed devise gem

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -1,16 +1,9 @@
 class Developer < ApplicationRecord
     has_many :projects
     has_many :clients, through: :projects
-    validates :name, presence: {message: "must be filled in!"}
-    validates :password, presence: :true
+    validates :name, presence: true
+    # validates :password, presence: :true
 
     accepts_nested_attributes_for :projects, reject_if: proc {|attributes| attributes['name'].blank? || attributes['description'].blank?}
-
-    # def projects_attributes=(projects_attributes)
-    #     projects_attributes.values.each do |atts|
-    #         if atts[:name] !="" && atts[:description] !=""
-    #             self.projects.build(atts)
-    #         end
-    #     end
-    # end
+    
 end
