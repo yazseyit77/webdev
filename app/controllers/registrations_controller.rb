013f35a489d57c79ba2fd73ffccff8cbd328e07e
yazseyit77 <yazgeldi.seyidov@gmail.com>
2019-11-10 21:04:55 -0800

    fixed some forms broken wiht bootstrap

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,9 +1,10 @@
 class RegistrationsController < Devise::RegistrationsController
-
+    before_action :configure_sign_up_params, if: :devise_controller?
 
     private 
-    def signup_params
-        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    def configure_sign_up_params
+        # params.require(:user).permit(:name, :email, :password, :password_confirmation)
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     end
 
     def account_update_params
