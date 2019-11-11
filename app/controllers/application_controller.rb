278d32bc206c736a84b9ce1539cb972062a42713
yazseyit77 <yazgeldi.seyidov@gmail.com>
2019-11-10 21:41:47 -0800

    fixed signin w/ github after it broke with some code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,4 @@ class ApplicationController < ActionController::Base
     def home
         
     end
-
-    def after_sign_in_path_for(resource)
-        stored_location_for(resource) || home
-    end
 end
